203b6632f6f2861f8b51197b25b1486ff6070383
Jakub Kułak <jakub.kulak@gmail.com>
2013-05-24 15:06:32 +0200

    Engine superclass moved to protocol

---
+++ SubtitleSource.h
@@ -0,0 +1,13 @@
+//
+//  SubtitleSource.h
+//  WhatSub
+//
+//  Created by Jakub Kulak on 24.05.2013.
+//  Copyright (c) 2013 burningtomato.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol SubtitleSource <NSObject>
+
+@end
